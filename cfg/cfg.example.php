32ab6110f0226612bb089a9275f27fa636f469fd
Tomasz Strąk <tstrak@divante.pl>
2014-09-02 10:23:49 +0200

    config example

---
+++ cfg/cfg.example.php
@@ -0,0 +1,9 @@
+<?php
+
+  define('TOKEN',               '');
+
+
+  define('DB_HOST',               '');
+  define('DB_USER',               '');
+  define('DB_PASS',               '');
+  define('DB_NAME',               '');

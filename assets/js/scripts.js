f11382243072bc0ee418799861d5925566ea8cca
Tomasz Strąk <tstrak@divante.pl>
2014-09-04 10:43:20 +0200

    okejka multiclicking

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,6 +5,7 @@
             this.prepareLayout();
             this.startLayout();
             this.okejka();
+            this.fDisableSelection();
         },
         prepareLayout : function(){
 
@@ -20,17 +21,53 @@
                 });
         },
         okejka: function() {
-            $(document).on('click','.element-item',function(e){
+            var $counter = 0;
+
+            $(document).on('click','.element-item a',function(e){
                 e.preventDefault();
-                $(this).prepend('<div class="okejka"></div>');
+
+                if (e.target != this){
+                    this.counter = 0;
+                    console.log(e.target);
+                }
+
+                this.$counter++;
+                console.log(this.counter);
+                var $class='okejka';
+
+                if(this.counter > 5){
+                    $class = 'okejka ten';
+                }
+                if (this.counter > 15 ){
+                    $class = 'okejka hundred';
+                }
+                if (this.counter >= 25 ){
+                    $class = 'okejka max';
+                    this.counter = 100;
+                }
+                $(this).prepend('<div class="'+$class+'"></div>');
+
                 $('.okejka').animate({
                     marginTop: "-=100px",
                     opacity: 0
                 },1000,function(){
-
+                    $(this).remove();
+                    setTimeout(function(){
+                       this.counter --;
+                    }, 3000);
                 });
                 //todo ajax
             });
+        },
+        fDisableSelection : function() {
+            $.fn.disableSelection = function() {
+                return this
+                         .attr('unselectable', 'on')
+                         .css('user-select', 'none')
+                         .on('selectstart', false);
+            };
+            $('img').disableSelection();
+            $('a').disableSelection();
         }
 
     }
@@ -38,3 +75,5 @@
         window.Kwas.init();
     });
 }(jQuery, window, document));
+
+
